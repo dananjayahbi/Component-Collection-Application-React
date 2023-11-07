b32e57eec209c34b1ed4cb85c737b1185921c554
djd <dananjayahbi@gmail.com>
2023-11-08 00:16:36 +0530

    checkpoint commit

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Component from '../components/SideBar'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Component />
+      Dashboard
+    </div>
   )
 }
 
